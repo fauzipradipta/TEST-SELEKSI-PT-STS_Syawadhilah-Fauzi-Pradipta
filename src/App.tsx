ce03eaf9c1994f0759bb6e43c449d19acb534963
Syawadhilah Pradipta <dipta0988@gmail.com>
2025-05-11 14:14:27 +0700

    setup vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RegisterPage from './pages/register/RegisterPage'
+import LoginPage from './pages/login/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +30,14 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,13 @@
+
+
+const LoginPage = () => {
+    return (
+        <div>
+        <h1>Login Page</h1>
+        <p>Please enter your credentials to log in.</p>
+        {/* Add your login form here */}
+        </div>
+    );
+}   
+
+export default LoginPage;
